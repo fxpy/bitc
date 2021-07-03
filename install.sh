b3f06841a6f59bef8be8e5fcc8b6e82154cba32b
fxpy <58055522+fxpy@users.noreply.github.com>
2021-07-03 22:54:50 +0300

    Update install.sh
    fix

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ rustup default stable
 rustup update nightly-2021-07-02
 #rustup update stable
 rustup target add wasm32-unknown-unknown --toolchain nightly-2021-07-02
-WASM_BUILD_TOOLCHAIN=nightly-2021-07-02 cargo build --release
+#WASM_BUILD_TOOLCHAIN=nightly-2021-07-02 cargo build --release
 rustup update
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
@@ -22,7 +22,8 @@ rustup target add wasm32-unknown-unknown --toolchain nightly
 cd $HOME
 git clone https://github.com/bit-country/Bit-Country-Blockchain.git
 cd Bit-Country-Blockchain
-git pull origin master
-# git checkout bfece87795f3b4bd4be225989af2ed717fbf9f8c
+git checkout 11f2b0ec216c7ce30008ec5a233cfe6baef8160d
+#git pull origin master
+#git checkout bfece87795f3b4bd4be225989af2ed717fbf9f8c
 ./scripts/init.sh
 cargo build --release --features=with-bitcountry-runtime
