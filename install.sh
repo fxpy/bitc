ae41ab91591362840202850b5c1e79608476b942
fxpy <58055522+fxpy@users.noreply.github.com>
2021-07-03 21:35:26 +0300

    Create install.sh
    init

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y cmake make clang pkg-config libssl-dev build-essential git jq libclang-dev clang curl libz-dev jq
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+source $HOME/.cargo/env
+
+rustup default stable
+
+rustup update nightly-2021-07-02
+#rustup update stable
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-07-02
+WASM_BUILD_TOOLCHAIN=nightly-2021-07-02 cargo build --release
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+#rustup default nightly-2021-07-02
+
+
+cd $HOME
+git clone https://github.com/bit-country/Bit-Country-Blockchain.git
+cd Bit-Country-Blockchain
+git pull origin master
+# git checkout bfece87795f3b4bd4be225989af2ed717fbf9f8c
+./scripts/init.sh
+cargo build --release --features=with-bitcountry-runtime
