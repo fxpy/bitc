3bbcff5fb85f2ac26bd9422e6ae3ea1ebc08cfc6
fxpy <58055522+fxpy@users.noreply.github.com>
2021-10-13 23:44:27 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
 systemctl stop bitcountryd.service
 
 mv /$HOME/Bit-Country-Blockchain/target/release/bitcountry-node /$HOME/Bit-Country-Blockchain/target/release/bitcountry-node.old 
 wget -O /$HOME/Bit-Country-Blockchain/target/release/bitcountry-node https://github.com/bit-country/Metaverse-Network/releases/download/v0.0.1/metaverse-node
 chmod +x /$HOME/Bit-Country-Blockchain/target/release/bitcountry-node
 
-systemctl restart bitcountryd
+systemctl restart bitcountryd.service
