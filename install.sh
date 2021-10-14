0020f15da077e319e62f9c97fdb7e3c085856c7c
fxpy <58055522+fxpy@users.noreply.github.com>
2021-10-14 23:44:45 +0300

    Update install.sh
    update to metaverse-node

--- install.sh
+++ install.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+read -p "Your bitcountry node name: " BITC_NODE
+echo 'export BITC_NODE="'${BITC_NODE}' | MMS"' >> $HOME/.bash_profile
+. $HOME/.bash_profile
+
+
 sudo apt update
 sudo apt install -y cmake make clang pkg-config libssl-dev build-essential git jq libclang-dev clang curl libz-dev jq
 
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-
 source $HOME/.cargo/env
-
-rustup default stable
-
-#rustup update nightly-2021-07-02
-#rustup update stable
-#rustup target add wasm32-unknown-unknown --toolchain nightly-2021-07-02
-#WASM_BUILD_TOOLCHAIN=nightly-2021-07-02 cargo build --release
-rustup update
-#rustup update nightly
-#rustup target add wasm32-unknown-unknown --toolchain nightly
-#rustup default nightly-2021-07-02
+rustup update nightly
+rustup update stable
+rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup default nightly
 
 
-#cd $HOME
-#git clone https://github.com/bit-country/Bit-Country-Blockchain.git
-#cd Bit-Country-Blockchain
-#git checkout 11f2b0ec216c7ce30008ec5a233cfe6baef8160d
-#git pull origin master
-#git checkout bfece87795f3b4bd4be225989af2ed717fbf9f8c
-#./scripts/init.sh
-#cargo build --release --features=with-bitcountry-runtime
+git clone https://github.com/bit-country/Metaverse-Network.git
+cd Metaverse-Network
+git checkout tewai-v0.0.1
+
+
+cargo build --release --features=with-tewai-runtime
+
+
+echo "[Unit]
+Description=Bit.Country Node
+After=network.target
+[Service]
+User=root
+WorkingDirectory=$HOME
+ExecStart=$HOME/Metaverse-Network/target/release/metaverse-node --chain tewai --validator --name '${BITC_NODE}' --bootnodes /ip4/13.239.118.231/tcp/30344/p2p/12D3KooW9rDqyS5S5F6oGHYsmFjSdZdX6HAbTD88rPfxYfoXJdNU --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' --pruning archive
+Restart=always
+RestartSec=3
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/bitcountryd.service
+
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+
+sudo systemctl enable bitcountryd
+sudo systemctl restart bitcountryd
