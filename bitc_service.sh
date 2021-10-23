c93c7ddb9a62bc617becadc58d544c8349db73ef
fxpy <58055522+fxpy@users.noreply.github.com>
2021-10-23 14:51:15 +0300

    Create bitc_service.sh
    init

---
+++ bitc_service.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read -p "Your bitcountry node name: " BITC_NODE
+echo 'export BITC_NODE="'${BITC_NODE}' | MMS"' >> $HOME/.bash_profile
+. $HOME/.bash_profile
+
+echo "[Unit]
+Description=Bit.Country Node
+After=network.target
+[Service]
+User=root
+WorkingDirectory=$HOME
+ExecStart=$HOME/Metaverse-Network/target/release/metaverse-node --chain tewai --validator --name '${BITC_NODE}' --bootnodes /ip4/13.239.118.231/tcp/30344/p2p/12D3KooW9rDqyS5S5F6oGHYsmFjSdZdX6HAbTD88rPfxYfoXJdNU --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' --pruning archive
+Restart=always
+RestartSec=3
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/bitcountryd.service
+
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+
+sudo systemctl enable bitcountryd
+sudo systemctl restart bitcountryd
+
+sleep 3
+
+systemctl status bitcountryd
