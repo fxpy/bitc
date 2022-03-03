814844d235359626d2ab52fc3f5dc1e6fbfd14b6
fxpy <58055522+fxpy@users.noreply.github.com>
2022-03-04 02:50:10 +0800

    Update update.sh
    update to 0.0.3

--- update.sh
+++ update.sh
@@ -4,20 +4,16 @@ sudo apt update
 sudo apt upgrade -y
 
 systemctl stop bitcountryd.service
-systemctl stop metaversed.service
 
 mv $HOME/Bit-Country-Blockchain/target/release/bitcountry-node $HOME/Bit-Country-Blockchain/target/release/bitcountry-node.oldest
-mv $HOME/Bit-Country-Blockchain/target/release/metaverse-node $HOME/Bit-Country-Blockchain/target/release/metaverse-node.old
+mv $HOME/Bit-Country-Blockchain/target/release/metaverse-node $HOME/Bit-Country-Blockchain/target/release/metaverse-node.oldest
 mv $HOME/Metaverse-Network/target/release/metaverse-node $HOME/Metaverse-Network/target/release/metaverse-node.old
 
-wget -O $HOME/Metaverse-Network/target/release/metaverse-node https://github.com/bit-country/Metaverse-Network/releases/download/tewai-release-277/metaverse-node-tewai-v0.0.2
-wget -O $HOME/Bit-Country-Blockchain/target/release/bitcountry-node https://github.com/bit-country/Metaverse-Network/releases/download/tewai-release-277/metaverse-node-tewai-v0.0.2
+wget -O $HOME/Metaverse-Network/target/release/metaverse-node https://github.com/fxpy/bitc/releases/download/v0.0.3/metaverse-node
+wget -O $HOME/Bit-Country-Blockchain/target/release/bitcountry-node https://github.com/fxpy/bitc/releases/download/v0.0.3/metaverse-node
 
 chmod +x $HOME/Metaverse-Network/target/release/metaverse-node
 chmod +x $HOME/Bit-Country-Blockchain/target/release/bitcountry-node
 
 systemctl restart bitcountryd.service
-systemctl restart metaversed.service
-
-systemctl status metaversed.service
 systemctl status bitcountryd.service
